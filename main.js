eb8dde744e0644f2d1a4f1274ff8c16a1a617076
HungyeeLinS3790956 <S3790956@student.rmit.edu.au>
2022-12-31 12:55:07 +0800

    fix header route info

--- main.js
+++ main.js
@@ -1,11 +1,92 @@
-window.addEventListener('load',()=>{
-    // document.querySelector('.header_rolling').innerText = 'Code Grid- Design tut'
+const AddHeader = () => {
+    let headerContent = document.getElementById('headerContent')
+    let allHeaderObject = [
+        {
+            headerName: 'Home',
+            elements:[]
+        },
+        {
+            headerName: 'Github Project',
+            elements: [
+                {
+                    route: 'https://github.com/celim1962/jschurch',
+                    name: 'JS Church'
+                },
+                {
+                    route: 'https://github.com/celim1962/PharmacyList',
+                    name: 'Pharmacy List'
+                }
+            ]
+        },
+        {
+            headerName: 'Social Media',
+            elements: [
+                {
+                    route: 'https://www.facebook.com/profile.php?id=100003635168406',
+                    name: 'FaceBook'
+                },
+                {
+                    route: 'https://www.instagram.com/celim1962/',
+                    name: 'Instagram'
+                }
+            ]
+        }
+    ]
 
+
+    allHeaderObject.map(item=>{
+        let div = document.createElement('div')
+        div.className = 'Item'
+
+        let btn = document.createElement('button')
+        btn.innerText = item.headerName
+
+        let ul = document.createElement('ul')
+
+        item.elements.map(element=>{
+            let li = document.createElement('li')
+            let a = document.createElement('a')
+
+            a.href = element.route
+            a.innerText = element.name
+
+            li.appendChild(a)
+            ul.appendChild(li)
+        })
+
+        div.appendChild(btn)
+        div.append(ul)
+
+        headerContent.appendChild(div)
+
+    })
+}
+
+
+
+
+const BaffleEffect = () => {
     const text = baffle('.header_rolling')
     text.set({
-                characters: '█>█ █▓▓▓▓ /▓█<▒ ░▓< ▓▓░█/ █░▒░ ▓/> ▓▒▓░ ▒▒▓▓',
-                speed:80
-            });
+        characters: '█>█ █▓▓▓▓ /▓█<▒ ░▓< ▓▓░█/ █░▒░ ▓/> ▓▒▓░ ▒▒▓▓',
+        speed: 80
+    });
     text.start()
-    text.reveal(1800);
+    text.reveal(1400);
+}
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('load', () => {
+    BaffleEffect()
+    AddHeader()
 })
+
